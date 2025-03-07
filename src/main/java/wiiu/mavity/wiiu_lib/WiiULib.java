86d8c5598685a8b6539f26c6184d754aeb5212bd
AngerVon2468 <136974526+AngerVon2468@users.noreply.github.com>
2025-03-07 19:40:17 +1100

    Strip main class down to as little as possible

--- src/main/java/wiiu/mavity/wiiu_lib/WiiULib.java
+++ src/main/java/wiiu/mavity/wiiu_lib/WiiULib.java
@@ -2,10 +2,4 @@ package wiiu.mavity.wiiu_lib;
 
 import net.neoforged.fml.common.Mod;
 
-@Mod(WiiULib.MOD_ID)
-public class WiiULib {
-
-    public static final String MOD_ID = "wiiu_lib";
-
-    public WiiULib() {}
-}
+@Mod("wiiu_lib") public class WiiULib {}
